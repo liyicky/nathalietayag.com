b5f9e2533d9a92db8f11592de4f2901ced425c44
liyicky <liyicky@gmail.com>
2015-08-29 13:53:06 -0400

    needs more cowbell

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,27 @@
 $(document).ready(function () {
 
-    var navbar = "#slide-nav"
-    var toggler = '.nav-toggle';
+    var navbar = "#slide-nav";
+    var navmenu = "#slidemenu";
+    var toggler = ".nav-toggle";
+    var pagewrapper = "#page-content";
+    var menuwidth = "100%";
+    var slidewidth = "80%";
+    var menuneg = "-100%";
+    var slideneg = "-80%";
+
 
     $(toggler).click(function() {
-      $(navbar).toggle();
+      // $(navbar).toggle("slow");
+      //
+      var selected = $(this).hasClass("slide-active");
+
+      $(navmenu).stop().animate({
+        left: selected ? menuneg : "0px"
+      });
+
+      $(this).toggleClass("slide-active", !selected);
+      $("#slidemenu").toggleClass("slide-active");
+      $("#page-content, .nav, body").toggleClass("slide-active");
     });
 
 
